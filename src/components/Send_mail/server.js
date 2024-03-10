5867e61312081a58b26fcc77922d2150edd85d63
swerg101 <sergeidunaev2@gmail.com>
2024-03-10 18:26:26 +0300

    UPD 4.3

--- src/components/Send_mail/server.js
+++ src/components/Send_mail/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 const transporter = nodemailer.createTransport({
     host: 'smtp.yandex.ru',
     port: 465,
-    secure: true,
+    secure: false,
     auth: {
         user: 'dunaevspb@yandex.ru',
         pass: 'sdpwhgkunarwvnqa'
