8fb3202d49a84c5e8627db855841def1a6e73a6c
swerg101 <sergeidunaev2@gmail.com>
2024-03-12 10:15:15 +0300

    UPD 4.3

--- src/components/Goods/testJsonAutoUpdate.js
+++ src/components/Goods/testJsonAutoUpdate.js
@@ -1,39 +1,77 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+//
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'POST' && req.url === '/sheets') {
+//         console.log('request is accept!')
+//         let body = '';
+//         req.on('data', (chunk) => {
+//             body += chunk.toString();
+//         });
+//         req.on('end', () => {
+//             try {
+//                 const data = JSON.parse(body);
+//                 // Записываем данные в файл data.json
+//                 fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+//                     if (err) {
+//                         console.error(err);
+//                         res.writeHead(500, { 'Content-Type': 'text/plain' });
+//                         res.end('Internal Server Error');
+//                     } else {
+//                         res.writeHead(200, { 'Content-Type': 'text/plain' });
+//                         res.end('File saved successfully');
+//                     }
+//                 });
+//             } catch (error) {
+//                 console.error(error);
+//                 res.writeHead(400, { 'Content-Type': 'text/plain' });
+//                 res.end('Bad Request');
+//             }
+//         });
+//     } else {
+//         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//         res.end('Not Found');
+//     }
+// });
+//
+// const PORT = 3600;
+// server.listen(PORT, () => {
+//     console.log(`Сервер для принятия запросов от Google Sheets настроен на порту: ${PORT}`);
+// });
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'POST' && req.url === '/sheets') {
-        let body = '';
-        req.on('data', (chunk) => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            try {
-                const data = JSON.parse(body);
-                // Записываем данные в файл data.json
-                fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-                    if (err) {
-                        console.error(err);
-                        res.writeHead(500, { 'Content-Type': 'text/plain' });
-                        res.end('Internal Server Error');
-                    } else {
-                        res.writeHead(200, { 'Content-Type': 'text/plain' });
-                        res.end('File saved successfully');
-                    }
-                });
-            } catch (error) {
-                console.error(error);
-                res.writeHead(400, { 'Content-Type': 'text/plain' });
-                res.end('Bad Request');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3600;
+
+app.use(express.json());
+app.use(cors());
+app.post('/sheets', async (req, res) => {
+    try {
+        console.log('request is processed')
+        const data = req.body;
+
+        app.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('File saved successfully');
             }
         });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
+
+        res.send('kermet.JSON is Update!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
     }
 });
 
-const PORT = 3600;
-server.listen(PORT, () => {
+app.listen(PORT, (err) => {
+    if (err) {
+        return console.log(err);
+    }
     console.log(`Сервер для принятия запросов от Google Sheets настроен на порту: ${PORT}`);
 });
